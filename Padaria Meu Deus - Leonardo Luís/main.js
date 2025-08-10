033ca9c021a9e4da3a5fe12607235394c5af8d7f
Leonardo Luís <kantodoleo@outlook.com>
2025-08-09 22:04:10 -0300

    Add files via upload

---
+++ Padaria Meu Deus - Leonardo Luís/main.js
@@ -0,0 +1,33 @@
+MyForm = document.getElementById("formlogin");
+
+MyForm.addEventListener("submit", function(event){
+
+    const formData = new FormData(MyForm);
+
+    fetch('login.php', {
+    method: 'POST',
+    body: formData
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+    event.preventDefault();
+});
+
+MyFormReserva = document.getElementById("formreserva");
+
+MyFormReserva.addEventListener("submit", function(event){
+
+    const formData = new FormData(MyFormReserva);
+
+    fetch('reserva.php', {
+    method: 'POST',
+    body: formData
+    })
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+    event.preventDefault();
+});
